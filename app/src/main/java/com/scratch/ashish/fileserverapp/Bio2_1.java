9eddf020b8fe9fa815a2661328c17e6d65b39e75
zingu12345 <ntgupta866@gmail.com>
2016-10-19 22:07:33 +0530

    after

---
+++ app/src/main/java/com/scratch/ashish/fileserverapp/Bio2_1.java
@@ -0,0 +1,8 @@
+package com.scratch.ashish.fileserverapp;
+
+/**
+ * Created by ashish on 19/10/16.
+ */
+
+public interface Bio2_1 {
+}
--- app/src/main/java/com/scratch/ashish/fileserverapp/DataAdapter.java
+++ app/src/main/java/com/scratch/ashish/fileserverapp/DataAdapter.java
@@ -1,9 +1,13 @@
 package com.scratch.ashish.fileserverapp;
 
 import android.app.DownloadManager;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +18,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static android.R.attr.action;
+import static android.content.Context.DOWNLOAD_SERVICE;
 
 /**
  * Created by ashish on 23/8/16.
@@ -29,12 +36,14 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> im
 
 
 
+
     public DataAdapter(ArrayList<AndroidVersions> android, Context context) {
         this.android = android;
         this.context = context;
 
 
     }
+
 
 
 
@@ -82,6 +91,7 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> im
 
 
 
+
         public ViewHolder(View view) {
             super(view);
 
@@ -105,15 +115,18 @@ public class DataAdapter extends RecyclerView.Adapter<DataAdapter.ViewHolder> im
             position = getAdapterPosition();
             //System.out.println(position);
             System.out.println(list.get(position));
-            String servicestring = context.DOWNLOAD_SERVICE;
+            String servicestring = DOWNLOAD_SERVICE;
             DownloadManager downloadmanager;
             downloadmanager = (DownloadManager) context.getSystemService(servicestring);
             Uri uri = Uri
                     .parse(list.get(position));
             DownloadManager.Request request = new DownloadManager.Request(uri);
             downloadmanager.enqueue(request);
+
             Toast toast = Toast.makeText(context,"Your file is now downloading...", (int) 0.3);
             toast.show();
+
+
             }
 
 
