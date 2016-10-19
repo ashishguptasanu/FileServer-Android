1bb2ddeccad2f9d431941f24f25097fd418de98e
zingu12345 <ntgupta866@gmail.com>
2016-10-20 02:32:31 +0530

    after

--- app/src/main/java/com/scratch/ashish/fileserverapp/Bio5_1.java
+++ app/src/main/java/com/scratch/ashish/fileserverapp/Bio5_1.java
@@ -1,8 +1,13 @@
 package com.scratch.ashish.fileserverapp;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by ashish on 20/10/16.
  */
 
 public interface Bio5_1 {
+    @GET("/rufly/Json/Biotech/4rthyear/4_3.txt")
+    Call<JSONResponse> getJSON();
 }
