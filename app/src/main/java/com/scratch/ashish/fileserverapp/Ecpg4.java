6ba2273d794303e8a3a02bb436c2c5ff41700d64
zingu12345 <ntgupta866@gmail.com>
2016-10-26 22:59:22 +0530

    Notify

---
+++ app/src/main/java/com/scratch/ashish/fileserverapp/Ecpg4.java
@@ -0,0 +1,13 @@
+package com.scratch.ashish.fileserverapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by ashish on 26/10/16.
+ */
+
+public interface Ecpg4 {
+    @GET("/rufly/Json/ecug/ecug1/ecug1_ES1.txt")
+    Call<JSONResponse> getJSON();
+}
--- app/src/main/java/com/scratch/ashish/fileserverapp/ecpg3.java
+++ app/src/main/java/com/scratch/ashish/fileserverapp/ecpg3.java
@@ -1,13 +1,33 @@
 package com.scratch.ashish.fileserverapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ecpg3 extends AppCompatActivity {
+    private Context context;
+    private RecyclerView recyclerView;
+    private ArrayList<AndroidVersions> data;
+    private DataAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +35,82 @@ public class ecpg3 extends AppCompatActivity {
         setContentView(R.layout.activity_ecpg3);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        View parentLayout = findViewById(android.R.id.content);
+        Snackbar.make(parentLayout,"Please Wait, Loading..", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+        initViews();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Next Update: Saturday", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+    }
+    private void initViews(){
+        recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view_ecug2_sub3);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        //recyclerView.setOnClickListener(new View.OnClickListener() {
+        //@Override
+        //public void onClick(View view) {
+        //int position = recyclerView.getAdapter().get
+        // }
+        // });
+        loadJSON();
+    }
+    private void loadJSON(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://s3.ap-south-1.amazonaws.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Ecpg3 request = retrofit.create(Ecpg3.class);
+        Call<JSONResponse> call = request.getJSON();
+        call.enqueue(new Callback<JSONResponse>() {
+            @Override
+            public void onResponse(Call<JSONResponse> call, Response<JSONResponse> response) {
+
+                JSONResponse jsonResponse = response.body();
+                data = new ArrayList<>(Arrays.asList(jsonResponse.getAndroid()));
+                adapter = new DataAdapter(data, ecpg3.this);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onFailure(Call<JSONResponse> call, Throwable t) {
+                Log.d("Error",t.getMessage());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent_setting = new Intent(this, Settings.class);
+            startActivity(intent_setting);
+            return true;
+        }
+        else if(id == R.id.about) {
+            Intent intent_about = new Intent(this, About.class);
+            startActivity(intent_about);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
